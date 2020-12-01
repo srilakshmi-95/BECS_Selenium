f9ff2b8910aafe43ab05f3873d4de7e9b36cfdb5
Srilakshmi <srilakshmi.shivanand@gmail.com>
2020-12-01 17:40:28 +0530

    Added:initalize and closeBrowser method

--- src/main/java/com/qa/BECS/pages/BasePage.java
+++ src/main/java/com/qa/BECS/pages/BasePage.java
@@ -1,13 +1,15 @@
 package com.qa.BECS.pages;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 
 import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class BasePage {
-   public WebDriver driver;
-   public Properties prop;
+   public  WebDriver driver;
+   public static Properties prop;
 
 public BasePage(){
    try{
@@ -19,9 +21,26 @@ public BasePage(){
       e.printStackTrace();
    }
 }
-public static void initialize(){
+   public BasePage(WebDriver driver){
+      this.driver = driver;
+   }
 
+public static void initialize(){
+String browsername = prop.getProperty("browser");
+if(browsername.equals("chrome")){
+   WebDriverManager.chromedriver().setup();
+}else if(browsername.equals("firefox")){
+   WebDriverManager.firefoxdriver().setup();
+}
+   driver.manage().deleteAllCookies();
+   driver.manage().window().maximize();
+   driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+   driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+   driver.get(prop.getProperty("url"));
 }
 
+public static void closeBrowser(){
+   driver.quit();
+}
 
 }
--- src/main/java/com/qa/BECS/pages/LoginPage.java
+++ src/main/java/com/qa/BECS/pages/LoginPage.java
@@ -1,4 +1,6 @@
 package com.qa.BECS.pages;
 
 public class LoginPage {
+
+
 }
