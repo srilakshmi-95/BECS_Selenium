c261bd01ad3b3f35ad639f734e6096a81ea67998
Srilakshmi <srilakshmi.shivanand@gmail.com>
2020-12-01 19:04:54 +0530

    First commit

--- src/main/java/com/qa/BECS/pages/SearchPage.java
+++ src/main/java/com/qa/BECS/pages/SearchPage.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class SearchPage extends BasePage {
-    @FindBy(xpath = "//input[@id='ctl00_TopSearch1_txtSearch']")
-    WebElement search;
+    @FindBy(xpath = "//body/form[@id='aspnetForm']/div[@id='site-wrapper']/div[2]/div[3]/div[2]/div[1]/div[4]/div[5]/a[1]/input[1]")
+    WebElement buy;
 
     //Initializing the page object
     public SearchPage(){
